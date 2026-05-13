c68c10826aa6e4b7433349c26c4336273631052c
Wejdan9-8bb <wejdanalqhatani91@gmail.com>
2026-05-13 21:07:07 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
