0276715f9f2d0b225ce15407f111b4a29605ff38
Wejdan9-8bb <wejdanalqhatani91@gmail.com>
2026-05-13 15:48:37 +0300

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using LibraryMgt.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryMgt
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+
+        public DbSet<UserInfo> UserInfoes {  get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authores { get; set; }
+        public DbSet<Borrower> Borrowers { get; set; }
+        public DbSet<Borrow> Borrows { get; set; }
+        
+    }
+}
