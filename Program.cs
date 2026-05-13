9a82a6bef1fa6b7377f11bddb5ebeb5e03e3df43
Wejdan9-8bb <wejdanalqhatani91@gmail.com>
2026-05-13 17:10:52 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,8 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
-option.UseSqlServer(builder.Configuration.GetConnectionString("DefaltConnection"), b => b.MigrationsAssembly("LibraryMgt")));
-
+option.UseSqlite("Data Source=library.db");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
